d7fa8972e8403b25860c699fc198792c0570b7a8
fahadrahmanch <ff865880@gmail.com>
2025-08-01 00:29:04 +0530

    useNavigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Counter from './Counter'
 import StopWatch from './stopWatch'
 import Check from './useRef'
+import Home from './home'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
@@ -8,9 +10,16 @@ function App() {
 
   return (
     <>
-     {/* <Counter/> */}
-     {/* <StopWatch/> */}
-     <Check/>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/counter' element={<Counter/>}/>
+      <Route path='/stopwatch' element={<StopWatch/>}/>
+      <Route path='/check' element={<Check/>}/>
+    </Routes>
+    </BrowserRouter>
+     
+    
     </>
   )
 }
---
+++ src/home.jsx
@@ -0,0 +1,12 @@
+import { useNavigate } from "react-router-dom"
+const Home=()=>{
+    const navigate=useNavigate()
+    return(
+        <>
+        <button onClick={()=>navigate('/counter')}>countet App</button><br/><br></br>
+        <button onClick={()=>navigate('/stopwatch')}>stopwatch App</button><br/><br></br>
+        <button onClick={()=>navigate('/check')}>check App</button><br/><br></br>
+        </>
+    )
+}
+export default Home
